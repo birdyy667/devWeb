b84a430b5dfbc83cbd393a2fe0074d0a568f43eb
Ffoo66 <130569354+Ffoo66@users.noreply.github.com>
2025-04-13 01:10:11 +0200

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import Register from './pages/Register';
 import Login from './pages/Login'; 
 import Dashboard from './pages/Dashboard';
-import Recherche from './pages/Recherche';
 import Navbar from './components/Navbar'; 
 import PrivateRoute from './components/PrivateRoute';
 import AjoutObjets from './components/AjoutObjets';
@@ -13,6 +12,8 @@ import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import ResetSuccess from './pages/ResetSuccess';
 import GestionObjets from './pages/GestionObjets';
+import Recherche from './pages/Recherche';
+import Objet from './pages/Objet';
 
 
 
@@ -31,8 +32,9 @@ function App() {
 
       <Route path="/reset-success" element={<ResetSuccess />} />
       <Route path="/reset-password/:token" element={<ResetPassword />} />      
-      <Route path="/mot-de-passe-oublie" element={<ForgotPassword />} />      
-      <Route path="/recherche" element={<Recherche />} />                                                                                                                                                                                                           
+      <Route path="/mot-de-passe-oublie" element={<ForgotPassword />} />                                                                                                                                                                                             
+      <Route path="/recherche" element={<Recherche />} />                                                                                                                                                                                                      
+      <Route path="/objet" element={<Objet />} />                                                                                                                                                                                          
         <Route path="/inscription" element={<Register />} />
         <Route path="/connexion" element={<Login />} />
         <Route path="/confirmation/:token" element={<Confirmation />} />
--- frontend/src/pages/Recherche.jsx
+++ frontend/src/pages/Recherche.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { io } from 'socket.io-client';
+// import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001'); // backend URL
+// const socket = io('http://localhost:3001'); // backend URL
 
 function Recherche() {
 	const navigate = useNavigate();
 	const [input, setInput] = useState('');
 	const [result, setResult] = useState(null);
+	const [formData, setFormData] = useState({
+		nom: '',
+		outils: ''
+	  });
 
+	/*
 	useEffect(
 		() => {
 			socket.on(
@@ -18,13 +23,14 @@ function Recherche() {
 			);
 		}
 	, []);
+	*/
 
 	useEffect(
 		() => {
 			const delayDebounce = setTimeout(
 				() => {
 					if (input.trim()) {
-					fetch(`http://localhost:3001/api/recherche/check?query=${input}`)
+						fetch(`http://localhost:3001/api/recherche/recherche?query=${input}`)
 						.then(res => res.json())
 						.then(data => setResult(data));
 					}
@@ -37,62 +43,36 @@ function Recherche() {
 		}
 	, [input]);
 
-	const handleSubmit = (id) => {
-		navigate(`/objet/${id}`);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+	
+		try {
+		  const res = await fetch('http://localhost:3001/api/findObject', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(formData)
+		  });
+	
+		  const data = await res.json();
+	
+		  if (res.ok) {
+			localStorage.setItem('userId', data.objet.idObjetConnecte);
+			navigate('/objet');
+		  } 
+		} catch (err) {
+		  console.error('Erreur côté client :', err);
+		}
 	};
 
 	const handleChange = (e) => {
 		const value = e.target.value;
 		setInput(value);
-		socket.emit('userInput', value);
-		/*
-		var test = 0;
-		var i = 0;
-		var found = document.createElement("div");
-		found.onClick={handleSubmit};
-		found.className="w-half px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500";
-		for (i in text){
-			if (test == 0){
-				found.innerHTML = "";
-			}
-			if (textarea.value.length > 2 && text[i].toLowerCase().includes(textarea.value.toLowerCase())){
-				if (test == 0){
-					link.innerHTML = "";
-				}
-				test = 1;
-				link.innerHTML += text[i] + "<br>";
-				sugg.appendChild(link);
-			}
-		}
-		
-		
-		
-		try {
-			const res = fetch('http://localhost:3001/api/recherche', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(formData)
-			});
-			
-			console.log(res);
-			const data = res;
-			
-			if (res.ok) {
-				setMessage(`🎉 Bienvenue, ${data.objetConnecte.nom} !`);
-				localStorage.setItem('objId', data.objetConnecte.idObjetConnecte);
-			}
-			else {
-				setMessage(`❌ Erreur : ${data.error}`);
-			}
-			
-		}
-		catch (err) {
-			console.error('Erreur côté client :', err);
-			setMessage('Erreur de connexion au serveur');
-		}
-		*/
+		//socket.emit('userInput', value);
+	};
+
+
+	const handleLoad = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 
 	return (
@@ -100,51 +80,53 @@ function Recherche() {
 			<div className="bg-white p-8 rounded-1xl shadow-lg w-full max-w-md">
 
 				<h2 className="text-2xl font-semibold mb-6 text-center">Recherche</h2>
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+				<div >
 					<input
 						name="nom"
 						type="text"
 						placeholder="Rechercher"
 						onChange={handleChange}
-						className="w-half px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+						className="w-max px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
 					/>
 				</div>
-				{
-					result && (
-						<p className="mt-4 text-center text-sm text-red-600">
-							{result}
-						</p>
-					)
-				}
 			</div>
 			<div className="bg-white p-8 rounded-1xl shadow-lg w-full max-w">
+				
 				{result && result.length > 0 && (
 					<div>
 						{result.map(
 							item =>(
-								<>
-									<p
-										className="w-half px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-									>
-										{item.nom}
-									</p>
-									<p
-										className="w-half px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-									>
-									{item.outils}
-									</p>
-									<button
-										type="submit"
-										onClick={() => navigate('/objet')}
-										className="w-max bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
-									>
-										Voir l'objet
-									</button>
+								<><form onSubmit={handleSubmit} className="space-y-4">
+								<input
+								  name="nom"
+								  type="text"
+								  placeholder={item.nom}
+								  onChange={handleLoad}
+								  disabled
+								  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+								/>
+								<input
+								  name="outils"
+								  type="text"
+								  placeholder={item.outils}
+								  onChange={handleLoad}
+								  disabled
+								  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+								/>
+								<button
+									type="submit"
+									onClick={handleSubmit}
+									className="w-max bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+								>
+									Voir l'objet
+								</button>
+							  </form>
 								</>
 							)
 						)}
 					</div>
 				)}
+				
 			</div>
 		</div>
 	);
